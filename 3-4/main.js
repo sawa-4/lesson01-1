2cefbb809bedaf715bbbdc8d122d89434c3d4cef
sawa-4 <30shi4s@gmail.com>
2021-05-05 18:24:54 +0900

    3-4　修正してもらった

--- 3-4/main.js
+++ 3-4/main.js
@@ -3,13 +3,23 @@ const a = (process.argv[2]);
 const b = (process.argv[3]);
 total = 0;
 function gyakuten(n){
-    n = Number(n.split("").reverse().join(""));
-    total += n
+    N = [];
+    for (let i = n.length - 1; i >= 0; i--) {
+        let m = n.charAt(i);
+        N.push(m);
+    }
+    N = Number(N.join(""));
+    total += N;
 }
 gyakuten(a);
 gyakuten(b);
 console.log(total);
-
+// ググったら出てきたやつ
+// function gyakuten(n){
+//     n = Number(n.split("").reverse().join(""));
+//     total += n
+// }
+// 自分で途中まで書いたやつ
 // function gyakuten(n){
 //     N = [];
 //     for (let i = 0; i < n.length; i++){
